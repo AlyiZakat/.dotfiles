286fbe7d396e07d973d9d283ac06628801004003
Faite <faiteoroleg@gmail.com>
2021-11-19 20:27:56 +0300

    update fish prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -4,6 +4,7 @@ function fish_prompt
     set -g __fish_git_prompt_color_dirtystate green
     set -g __fish_git_prompt_color_branch yellow
     set -g __fish_git_prompt_showupstream "informative"
+    set -g __fish_git_prompt_char_cleanstate "✓ "
     printf "%s" (fish_git_prompt)
     echo (set_color blue) (prompt_pwd) (set_color white)'>' (set_color normal)
 end
