588c8bf76a83061ba8337495f8dca6c0384925ed
AlyiZakat <zakatalyi@gmail.com>
2022-07-02 23:43:44 +0300

    added plugin nvimtree

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -5,5 +5,11 @@ return require('packer').startup(function()
 	use 'tomasiser/vim-code-dark'
 	use {'neoclide/coc.nvim', branch = 'release'}
 	use {'nvim-treesitter/nvim-treesitter', commit = '749f72d'} -- 15-01-22 work with Lua and PHP
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = {
+			'kyazdani42/nvim-web-devicons', -- optional, for file icons
+		}
+	}
 	-- use {'nvim-treesitter/nvim-treesitter', commit = '45d384e'} -- 19-02-22 work PHP not work Lua
 end)
