7bc528033d8ea6f901bccf07946819df5d4af62b
olsolk <63130155+olsolk@users.noreply.github.com>
2023-03-25 20:46:18 +0300

    refactor(neovim) use mason handler

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -40,61 +40,67 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
---local servers = { 'intelephense', 'sumneko_lua', 'tsserver', 'pyright' }
---for _, lsp in ipairs(servers) do
---lspconfig[lsp].setup {
---	on_attach = on_attach,
---	capabilities = capabilities,
---	single_file_support = true
---	}
---end
-
-lspconfig.lua_ls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    single_file_support = true,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim', 'use' }
+require("mason-lspconfig").setup_handlers({
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+    -- Next, you can provide targeted overrides for specific servers.
+    -- ["rust_analyzer"] = function ()
+    --     require("rust-tools").setup {}
+    -- end,
+    ["lua_ls"] = function()
+        lspconfig.lua_ls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim", "use" }
+                    }
+                }
             }
         }
-    }
-}
+    end,
+    ["intelephense"] = function()
+        lspconfig.intelephense.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            single_file_support = true,
+            init_options = {
+                globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
+            }
+        }
+    end,
+    ["pyright"] = function()
+        lspconfig.pyright.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            single_file_support = true
+        }
+    end,
+    ["gopls"] = function()
+        local util = require "lspconfig/util"
 
-lspconfig.intelephense.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    single_file_support = true,
-    init_options = {
-        globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
-    }
-}
-
-lspconfig.pyright.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    single_file_support = true
-}
-
-local util = require "lspconfig/util"
-
-lspconfig.gopls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { "gopls", "serve" },
-    filetypes = { "go", "gomod" },
-    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
+        lspconfig.gopls.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            cmd = { "gopls", "serve" },
+            filetypes = { "go", "gomod" },
+            root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+            settings = {
+                gopls = {
+                    analyses = {
+                        unusedparams = true,
+                    },
+                    staticcheck = true,
+                },
             },
-            staticcheck = true,
-        },
-    },
-}
+        }
+    end
+})
 
 -- luasnip setup
 local luasnip = require 'luasnip'
